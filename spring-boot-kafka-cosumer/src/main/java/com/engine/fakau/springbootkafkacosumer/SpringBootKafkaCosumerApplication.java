e9460ac385e965aed72df70679cd4fa8098025fb
Laurent Kafka M <laurentkafka123@gmail.com>
2022-08-08 11:45:27 -0400

    add kafka consumer service

---
+++ spring-boot-kafka-cosumer/src/main/java/com/engine/fakau/springbootkafkacosumer/SpringBootKafkaCosumerApplication.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.springbootkafkacosumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaCosumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaCosumerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-kafka-cosumer/src/test/java/com/engine/fakau/springbootkafkacosumer/SpringBootKafkaCosumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.springbootkafkacosumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaCosumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
