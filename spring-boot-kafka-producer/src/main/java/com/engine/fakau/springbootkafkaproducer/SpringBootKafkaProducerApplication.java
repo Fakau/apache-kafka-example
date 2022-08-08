34d0eaec7782116a3aa3b6fbb7270894a716e097
Laurent Kafka M <laurentkafka123@gmail.com>
2022-08-08 09:33:53 -0400

    init java producer

---
+++ spring-boot-kafka-producer/src/main/java/com/engine/fakau/springbootkafkaproducer/SpringBootKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.springbootkafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-kafka-producer/src/test/java/com/engine/fakau/springbootkafkaproducer/SpringBootKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.springbootkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
